749081037efa63b5d14b3e478a43a621d132ce7d
Philipp Höhne <MrLalelu@users.noreply.github.com>
2019-04-11 15:17:44 +0200

    updated back.ino code to handle incoming state over serial and either display flashing if open or an effect otherwise

--- back/back.ino
+++ back/back.ino
@@ -29,8 +29,6 @@
 
 #define PIN_BACKLIGHT 7
 #define NUM_LEDS_BACKLIGHT 149
-#define PIN_INSIDE 6
-#define NUM_LEDS_INSIDE 24
 
 #define NUM_EFFECTS 2
 #define NUM_LEDS_RUNNING 10     // the amount of LEDs that should be 
@@ -43,8 +41,8 @@
 #define NUM_COLORS 1
 
 // defining colors for the different liquors
-uint8_t colors[NUM_COLORS][3] = {{COLOR_PFEFFI}};//, {COLOR_JAEGERMEISTER},
-                                 //{COLOR_ROTER}, {COLOR_SAURER}, {COLOR_BLAU}};
+uint8_t colors[NUM_COLORS][3] = {{COLOR_PFEFFI}, {COLOR_JAEGERMEISTER},
+                                 {COLOR_ROTER}, {COLOR_SAURER}, {COLOR_BLAU}};
 
 int draws[NUM_DRAWS] = {DRAW1, DRAW2, DRAW3, DRAW4, DRAW5, DRAW6};
 int pins_draws[NUM_DRAWS] = {PIN_BUTTON_DRAW1, PIN_BUTTON_DRAW2, PIN_BUTTON_DRAW3,
@@ -107,15 +105,7 @@ void setup() {
         pinMode(pins_draws[i], INPUT);
     }
     pixels_backlight.begin();
-    pixels_inside.begin();
     pixels_backlight.show();
-    pixels_inside.show();
-
-    for (int i = 0; i < NUM_LEDS_INSIDE; i++) {
-        pixels_inside.setPixelColor(i, 0, 0, 255);
-    }
-
-    pixels_inside.show();
 
     srand(millis());
 }
@@ -139,34 +129,7 @@ float int_g = 0.;
 float int_b = 0.;
  
 void loop() {
-    for (int i = 0; i < NUM_LEDS_INSIDE; i++) {
-        pixels_inside.setPixelColor(i, 0, 255, 0);
-    }
-    // reading whether a drawer is open. Read NUM_READS to get rid of noise
-    for (int i = 0; i < NUM_DRAWS; i++) {
-        draw_open_count[i] = 0;
-    }
-
-    for (int i = 0; i < NUM_DRAWS; i++) {
-        draw_open_count[i] = 0;
-        for(int i2 = 0; i2 < NUM_READS; i2++) {
-            draw_open_count[i] += digitalRead(pins_draws[i]);
-        }
-        draw_open[i] = draw_open_count[i] / NUM_READS > 0.5;
-        Serial.print(draw_open_count[i] / NUM_READS);
-        Serial.print(" ");
-    }
-    Serial.println("");
-    delay(1000);
-    any_draw_open = false;
-
-    for(int i = 0; i < 6; i++) {
-        if (draw_open[i]) {    //TODO remove not after testing!!!
-            any_draw_open = true;
-            key_draw_open = i;
-            break;
-        }
-    }
+    any_draw_open = read_is_open();
 
     // if a draw is open set the backlight color to the color of the chosen
     // liqour and iluminate the draw (last 6 leds on the inside strip)
@@ -333,7 +296,6 @@ void loop() {
 
 
     pixels_backlight.show();
-    pixels_inside.show();
 
 }
 
